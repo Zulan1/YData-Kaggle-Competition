4a5d7316283babbdd3889a44408c1fc622c7cdc9
Amit Zulan <amitzulan@gmail.com>
2025-01-23 13:06:18 +0200

    implemented 1st draft of train

---
+++ app/argparser.py
@@ -0,0 +1,19 @@
+import argparse
+
+#define training arguments
+def get_train_args():
+    parser = argparse.ArgumentParser(description='Train a model')
+    parser.add_argument('--optuna-search', action='store_true', help='Whether to perform hyperparameter search')
+    parser.add_argument('--n-trials', type=int, default=100, help='number of trials for hyperparameter search')
+    parser.add_argument('--trial-epochs', type=int, default=100, help='Number of epochs per trial for hyperparameter search')
+    parser.add_argument('--scoring-method', type=str, default='f1', help='The metric to use for evaluation')
+    parser.add_argument('--model-type', type=str, default='RandomForest', help='The type of model to train')
+
+    parser.add_argument('--C', type=float, default=0, help='The regularization strength for Logistic Regression or SVM')
+
+    parser.add_argument('--n-estimators', type=int, default=100, help='The number of estimators for Random Forest')
+    parser.add_argument('--criterion', type=str, default='gini', help='The criterion for Random Forest')
+
+    parser.add_argument('--kernel', type=str, default='rbf', help='The kernel for SVM')
+
+    return parser.parse_args()
