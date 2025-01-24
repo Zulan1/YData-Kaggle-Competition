9eb4fdb878ab4a0148c847a2cc2b094afc1ed7e2
amitzulan <amitzulan@gmail.com>
2025-01-24 12:06:12 +0200

    wandb-init-patch

--- app/argparser.py
+++ app/argparser.py
@@ -5,15 +5,14 @@ def get_train_args():
     parser = argparse.ArgumentParser(description='Train a model')
     parser.add_argument('--optuna-search', action='store_true', help='Whether to perform hyperparameter search')
     parser.add_argument('--n-trials', type=int, default=100, help='number of trials for hyperparameter search')
-    parser.add_argument('--trial-epochs', type=int, default=100, help='Number of epochs per trial for hyperparameter search')
     parser.add_argument('--scoring-method', type=str, default='f1', help='The metric to use for evaluation')
-    parser.add_argument('--model-type', type=str, default='RandomForest', help='The type of model to train')
+    parser.add_argument('--model-type', type=str, default=None, help='The type of model to train')
 
-    parser.add_argument('--C', type=float, default=0, help='The regularization strength for Logistic Regression or SVM')
+    parser.add_argument('--C', type=float, default=None, help='The regularization strength for Logistic Regression or SVM')
 
-    parser.add_argument('--n-estimators', type=int, default=100, help='The number of estimators for Random Forest')
-    parser.add_argument('--criterion', type=str, default='gini', help='The criterion for Random Forest')
+    parser.add_argument('--n-estimators', type=int, default=None, help='The number of estimators for Random Forest')
+    parser.add_argument('--criterion', type=str, default=None, help='The criterion for Random Forest')
 
-    parser.add_argument('--kernel', type=str, default='rbf', help='The kernel for SVM')
+    parser.add_argument('--kernel', type=str, default=None, help='The kernel for SVM')
 
     return parser.parse_args()
