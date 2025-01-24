d718be9aead5a8bf93fd6d0ecca12e3d060a327c
Alexandra <pavalexa@gmail.com>
2025-01-24 17:47:45 +0200

    update constasnts

--- constants.py
+++ constants.py
@@ -1,18 +1,16 @@
 
 # Paths and filenames:
-DATA_PATH = 'notebooks/'
+DATA_PATH = 'data/'
 DEFAULT_RAW_TRAIN_FILE = 'train_dataset_full.csv'
 DEFAULT_EXTERNAL_RAW_TEST_FILE = 'X_test_1st_raw.csv'
-DEFAULT_TRAIN_FOLD_FILE = 'train_fold_'
-DEFAULT_VAL_FOLD_FILE = 'val_fold_'
 
 DEFAULT_TRAIN_SET_FILE = 'train.csv'
 DEFAULT_VAL_SET_FILE = 'val.csv'
 DEFAULT_TEST_SET_FILE = 'test.csv'
 
 COLUMNS_TO_DROP = ['product_category_2', 'city_development_index']
-DATETIME_COLUMN = ['DateTime']
-TARGET_COLUMN = ['is_click']
+DATETIME_COLUMN = 'DateTime'
+TARGET_COLUMN = 'is_click'
 
 #Column names groups:
 DEMOGRAPHICS = ['gender', 'age_level', 'city_development_index', 'user_group_id']
