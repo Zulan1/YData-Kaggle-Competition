ca45ba8272fd8b931047df736c3288387414cbc9
Yuval Dor <yuvaldor@Yuvals-MacBook-Pro.local>
2025-01-31 15:17:55 +0200

    Update pipeline and result processing

--- result.py
+++ result.py
@@ -2,15 +2,10 @@ import argparse
 from app.argparser import get_result_args
 from sklearn.metrics import f1_score
 from sklearn.metrics import confusion_matrix
+
 import pandas as pd
 import constants as cons
 import os
-
-def get_predictions(predictions_path):
-    return pd.read_csv(predictions_path)
-
-def get_labels(labels_path):
-    return pd.read_csv(labels_path)
 
 def main():
     args = get_result_args()
@@ -18,8 +13,8 @@ def main():
     predictions_path = os.path.join(args.input_path, f"predict_{run_id}/{cons.DEFAULT_PREDICTIONS_FILE}")
     predictions = pd.read_csv(predictions_path)
 
-    features_path = os.path.join(args.input_path, f"preprocess_{run_id}/{cons.DEFAULT_PROCESSED_TEST_FILE}")
-    features = pd.read_csv(features_path)
+    default_external_test_file = os.path.join(args.input_path, cons.DEFAULT_EXTERNAL_RAW_TEST_FILE)
+    features = pd.read_csv(default_external_test_file)
 
     df = pd.concat([features, predictions], axis=1)
     output_path = os.path.join(args.output_path, f"result_{run_id}")
